f0fcdfa27551ce9ae1786feb369ece130e093f09
Troy Flavell <hunter007nz@gmail.com>
2021-11-17 22:46:55 +1300

    Added logic for cloud file storage

---
+++ scoreboard/config/plugins.js
@@ -0,0 +1,13 @@
+module.exports = ({ env }) => ({
+  upload: {
+    providerOptions: {
+      bucket: 'project4-leaderboard',
+      region: 'us-east-1',
+      accessKeyId: 'AKIAIOSFODNN7EXAMPLE',
+      secretAccessKey: 'wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY',
+
+
+    }
+  }
+
+})
